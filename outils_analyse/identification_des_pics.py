e1b4d4c39359c246726402bcaec36f005583f998
Gérémy Michaud <gemic54@ulaval.ca>
2023-10-21 01:06:49 -0400

    Anal_leger

--- outils_analyse/identification_des_pics.py
+++ outils_analyse/identification_des_pics.py
@@ -4,7 +4,7 @@ from scipy.signal import find_peaks
 
 def determiner_indexes_maximums_scipy(valeurs: np.array, colonne_y: int,
                                       hauteur_minimum: int = None,
-                                      distance_minumum: int = None):
+                                      distance_minimum: int = None):
     """
     Cette méthode utilise la méthode find_peaks de scipy pour trouver les index de l'array correspondant aux maximums.
 
@@ -14,12 +14,12 @@ def determiner_indexes_maximums_scipy(valeurs: np.array, colonne_y: int,
                             deux pics pour guider l'algorithme de scipy
                             Cette valeur devrait être inférieur à la hauteur du plus grand pic.
                             de l'array numpy (<array.max())
-    :param distance_minumum:Optionnel! Celui-ci indique la distance minimale entre
+    :param distance_minimum:Optionnel! Celui-ci indique la distance minimale entre
                             deux pics pour guider l'algorithme de scipy
                             Cette valeur devrait être inférieur à la longueur totale
-                            de l,array numpy divisé par le nombre total de pics (<(array.shape[1]/nb de pics))
+                            de l'array numpy divisé par le nombre total de pics (<(array.shape[1]/nb de pics))
 
     :return: liste des index correspondant aux emplacements des maximums
     """
-    peaks, _ = find_peaks(valeurs[:, colonne_y], height=hauteur_minimum, distance=distance_minumum)
+    peaks, _ = find_peaks(valeurs[:, colonne_y], height=hauteur_minimum, distance=distance_minimum)
     return peaks
